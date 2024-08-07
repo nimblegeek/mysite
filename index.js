d3b3aa3427b9fbaf2eb1c88daca852386becfa86
Jonas Achouri Sihlén <62463923+nimblegeek@users.noreply.github.com>
2024-08-07 22:00:36 +0000

    cleaning

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const renderPage = require('./lib/renderPage');
-const marked = require('marked');
-const path = require('path');
+const express = require("express");
+const renderPage = require("./lib/renderPage");
+const marked = require("marked");
+const path = require("path");
 const app = express();
-const fs = require('fs');
-const hljs = require('highlight.js');
-const _ = require('lodash');
-const { Feed } = require('feed');
+const fs = require("fs");
+const hljs = require("highlight.js");
+const _ = require("lodash");
+const { Feed } = require("feed");
 
 const renderer = new marked.Renderer();
 
 // Pass html unescaped
-renderer.html = str => _.unescape(str);
+renderer.html = (str) => _.unescape(str);
 
 marked.setOptions({
   renderer: renderer,
@@ -29,37 +29,24 @@ const staticOpts = {
   maxAge: 365 * 24 * 60 * 60 * 1000,
 };
 
-app.use('/public', express.static(
-  path.join(__dirname, 'public'), staticOpts
-));
+app.use("/public", express.static(path.join(__dirname, "public"), staticOpts));
 
-app.use('/bitcoin', express.static(
-  path.join(__dirname, 'bitcoin.pdf'), staticOpts
-));
+app.use(
+  "/amasad",
+  express.static(path.join(__dirname, "public/images/amasad.jpg"), staticOpts),
+);
 
-app.use('/amasad', express.static(
-  path.join(__dirname, 'public/images/amasad.jpg'), staticOpts
-));
-
-app.use('/natural', express.static(
-  path.join(__dirname, 'public/natural.pdf'), staticOpts
-));
-
-app.use('/red', express.static(
-  path.join(__dirname, 'public/fbred.pdf'), staticOpts
-));
-
-const essaysDir = path.join(__dirname, 'essays');
+const essaysDir = path.join(__dirname, "essays");
 
 function parse(fpath) {
-  const post = fs.readFileSync(path.join(essaysDir, fpath), 'utf8');
-  const parts = post.split('---\n');
+  const post = fs.readFileSync(path.join(essaysDir, fpath), "utf8");
+  const parts = post.split("---\n");
   const json = parts.shift();
   const essay = (() => {
     try {
       return JSON.parse(json);
     } catch (e) {
-      console.error('error parsing json for essay', fpath);
+      console.error("error parsing json for essay", fpath);
       throw e;
     }
   })();
@@ -69,15 +56,16 @@ function parse(fpath) {
     throw new Error(`Invalid date: ${essay.date} - ${fpath}`);
   }
   essay.date = new Date(date);
-  essay.raw = parts.join('---\n');
+  essay.raw = parts.join("---\n");
   essay.content = marked(essay.raw);
-  essay.url = path.basename(fpath, '.md');
-  essay.description = essay.raw.slice(0, 200).replace(/\n/g, ' ') + '...';
+  essay.url = path.basename(fpath, ".md");
+  essay.description = essay.raw.slice(0, 200).replace(/\n/g, " ") + "...";
   return essay;
 }
 
-const essays = fs.readdirSync(essaysDir)
-  .filter(fpath => fpath[0] !== '.')
+const essays = fs
+  .readdirSync(essaysDir)
+  .filter((fpath) => fpath[0] !== ".")
   .map(parse)
   .sort((a, b) => b.date - a.date);
 
@@ -86,24 +74,26 @@ const feed = new Feed({
   description: "Essays",
   id: "https://amasad.me",
   link: "https://amasad.me",
-  image: "https://www.gravatar.com/avatar/03637ef1a5121222c8db0ed48c34e124.png?s=200",
-  favicon: "https://www.gravatar.com/avatar/03637ef1a5121222c8db0ed48c34e124.png?s=200",
+  image:
+    "https://www.gravatar.com/avatar/03637ef1a5121222c8db0ed48c34e124.png?s=200",
+  favicon:
+    "https://www.gravatar.com/avatar/03637ef1a5121222c8db0ed48c34e124.png?s=200",
   copyright: "All rights reserved 2019, Amjad Masad",
   generator: "amasad.me",
   feedLinks: {
     json: "https://amasad.me/json",
-    rss: "https://amasad.me/rss"
+    rss: "https://amasad.me/rss",
   },
   author: {
     name: "Amjad Masad",
     email: "amjad.masad@gmail.com",
-    link: "https://amasad.me"
-  }
+    link: "https://amasad.me",
+  },
 });
 
 for (const essay of essays) {
   if (essay.hidden) continue;
-  
+
   feed.addItem({
     title: essay.title,
     id: essay.id,
@@ -116,7 +106,7 @@ for (const essay of essays) {
       {
         name: "Amjad Masad",
         email: "amjad.masad@gmail.com",
-        link: "https://amasad.me"
+        link: "https://amasad.me",
       },
     ],
   });
@@ -124,27 +114,31 @@ for (const essay of essays) {
 
 feed.addCategory("Tech");
 
-app.use(function(req,res,next) {
-  res.header('X-Rob-Is-Awesom', true);
+app.use(function (req, res, next) {
+  res.header("X-Rob-Is-Awesom", true);
   next();
 });
 
-app.get('/json', (req, res) => {
+app.get("/json", (req, res) => {
   res.end(feed.json1());
 });
 
-app.get('/rss', (req, res) => {
+app.get("/rss", (req, res) => {
   res.end(feed.rss2());
 });
 
-app.get('/hello', (req, res) => res.end('hello world'));
+app.get("/hello", (req, res) => res.end("hello world"));
 
-app.get('/', (req, res) => {
-  const essayList = essays.filter(es => !es.hidden).map(es => (
-    `<li>
+app.get("/", (req, res) => {
+  const essayList = essays
+    .filter((es) => !es.hidden)
+    .map(
+      (es) =>
+        `<li>
        <a href="${es.url}">${es.title}</a>
-     </li>`
-  )).join('\n');
+     </li>`,
+    )
+    .join("\n");
 
   const html = `
   <article class="index postContent">
@@ -184,80 +178,79 @@ essays.forEach((es) => {
         </article>
         `;
 
-    const page = renderPage({
-      title: es.title,
-      description: es.description,
-      url: `/${es.url}`,
-      image: es.image,
-    }, html);
+    const page = renderPage(
+      {
+        title: es.title,
+        description: es.description,
+        url: `/${es.url}`,
+        image: es.image,
+      },
+      html,
+    );
     res.send(page);
   });
 });
 
-app.get('/about', (req, res) => {
-  fs.readFile(
-    path.join(__dirname, 'pages', 'about.md'),
-    (err, d) => {
-      if (err) {
-        res.status(500);
-        res.text('something went wrong :(');
-        return;
-      }
+app.get("/about", (req, res) => {
+  fs.readFile(path.join(__dirname, "pages", "about.md"), (err, d) => {
+    if (err) {
+      res.status(500);
+      res.text("something went wrong :(");
+      return;
+    }
 
-      res.set('Content-Type', 'text/html');
-      const content = marked(d.toString('utf8'));
-      const html = `<article class="postItem postContent">
+    res.set("Content-Type", "text/html");
+    const content = marked(d.toString("utf8"));
+    const html = `<article class="postItem postContent">
       ${content}
       </article>`;
 
-      const page = renderPage({}, html);
-      res.send(page);
-    }
-  );
+    const page = renderPage({}, html);
+    res.send(page);
+  });
 });
 
-app.get('/bio', (req, res) => {
-  fs.readFile(
-    path.join(__dirname, 'pages', 'bio.md'),
-    (err, d) => {
-      if (err) {
-        res.status(500);
-        res.text('something went wrong :(');
-        return;
-      }
+app.get("/bio", (req, res) => {
+  fs.readFile(path.join(__dirname, "pages", "bio.md"), (err, d) => {
+    if (err) {
+      res.status(500);
+      res.text("something went wrong :(");
+      return;
+    }
 
-      res.set('Content-Type', 'text/html');
-      const content = marked(d.toString('utf8'));
-      const html = `<article class="postItem postContent">
+    res.set("Content-Type", "text/html");
+    const content = marked(d.toString("utf8"));
+    const html = `<article class="postItem postContent">
       ${content}
       </article>`;
 
-      const page = renderPage({}, html);
-      res.send(page);
-    }
-  );
+    const page = renderPage({}, html);
+    res.send(page);
+  });
 });
 
 const redirections = {
-  '/2012/12/11/stuffjs': '/stuffjs',
-  '/2016/04/14/kierkegaard-and-entrepreneurship': '/kierkegaard',
-  '/2015/04/09/hello-world': '/hello-world',
-  '/2016/03/05/caching-and-promises': '/caching-promises',
-  '/2012/07/17/on-vms-in-javascript': '/js-vms',
-  '/2016/01/17/eval-as-a-service': '/eval-as-a-service',
-  '/2016/01/03/overcoming-intuition-in-programming': '/intuition',
-  '/2016/03/09/john-carmack-on-idea-generation': '/carmack',
-  '/2012/07/02/the-dark-side-of-functionprototypebind': '/bind',
-  '/2016/01/13/the-stoic-of-open-source': '/stoic-oss',
-  '/2014/03/09/lesser-known-javascript-debugging-techniques': '/debugging',
-  '/2014/03/16/why-im-excited-about-objectobserve': '/object-observe',
-  '/2015/10/31/javascript-async-functions-for-easier-concurrent-programming':
-    '/async-js',
-  '/2016/03/13/what-is-perfectionism-and-how-to-cure-it': '/perfectionism',
-  '/2014/01/18/introducing-waraby-mobile-optimized-arabic-search-web-app':
-    '/waraby',
-  '/2014/01/10/implementing-bret-victors-learnable-programming-has-never-been-easier': '/learnable',
-  '/2014/01/06/building-an-in-browser-javascript-vm-and-debugger-using-generators': '/js-debugger',
+  "/2012/12/11/stuffjs": "/stuffjs",
+  "/2016/04/14/kierkegaard-and-entrepreneurship": "/kierkegaard",
+  "/2015/04/09/hello-world": "/hello-world",
+  "/2016/03/05/caching-and-promises": "/caching-promises",
+  "/2012/07/17/on-vms-in-javascript": "/js-vms",
+  "/2016/01/17/eval-as-a-service": "/eval-as-a-service",
+  "/2016/01/03/overcoming-intuition-in-programming": "/intuition",
+  "/2016/03/09/john-carmack-on-idea-generation": "/carmack",
+  "/2012/07/02/the-dark-side-of-functionprototypebind": "/bind",
+  "/2016/01/13/the-stoic-of-open-source": "/stoic-oss",
+  "/2014/03/09/lesser-known-javascript-debugging-techniques": "/debugging",
+  "/2014/03/16/why-im-excited-about-objectobserve": "/object-observe",
+  "/2015/10/31/javascript-async-functions-for-easier-concurrent-programming":
+    "/async-js",
+  "/2016/03/13/what-is-perfectionism-and-how-to-cure-it": "/perfectionism",
+  "/2014/01/18/introducing-waraby-mobile-optimized-arabic-search-web-app":
+    "/waraby",
+  "/2014/01/10/implementing-bret-victors-learnable-programming-has-never-been-easier":
+    "/learnable",
+  "/2014/01/06/building-an-in-browser-javascript-vm-and-debugger-using-generators":
+    "/js-debugger",
 };
 
 Object.keys(redirections).forEach((key) => {
@@ -269,5 +262,5 @@ Object.keys(redirections).forEach((key) => {
 
 const port = 8000;
 app.listen(port, () => {
-  console.log('running on port', port);
+  console.log("running on port", port);
 });
